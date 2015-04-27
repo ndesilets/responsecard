e20518b3056f6458ac8baef54d65b565dd5ba231
Nic Desilets <nic@desilets.org>
2015-04-26 23:29:30 -0700

    Update nRF24L01.h

--- libraries/nRF24L01/nRF24L01.h
+++ libraries/nRF24L01/nRF24L01.h
@@ -1,25 +1,4 @@
-
-#define R_REGISTER 0x00
-#define W_REGISTER 0x20
-#define R_RX_PAYLOAD 0x61
-#define W_TX_PAYLOAD 0xA0
-#define FLUSH_TX 0xE1
-#define FLUSH_RX 0xE2
-#define REUSE_TX_PL 0xE3
-#define ACTIVATE 0x50
-#define R_RX_PL_WID 0x60
-#define W_ACK_PAYLOAD 0xA8
-#define W_TX_PAYLOAD_NO_ACK 0xB0
-#define NOP 0xFF
-
-
-#define CONFIG 0x00
-#define EN_AA 0x01
-#define EN_RXADDR 0x02
-#define SETUP_AW 0x03
-#define SETUP_RETR 0x04
-#define RF_CH 0x05
-#define RF_SETUP 0x06/*
+/*
     Copyright (c) 2007 Stefan Engelke <mbox@stefanengelke.de>
 	Portions Copyright (C) 2011 Greg Copeland
 
